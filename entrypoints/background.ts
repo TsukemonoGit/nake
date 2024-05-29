b6aa81fbbb7a2834772afb50f16fcd3dc7052578
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-05-29 15:34:16 +0900

    fix popup

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,33 +1,73 @@
 import { browser } from "wxt/browser";
+
 export default defineBackground(() => {
-  // browser.contextMenus.create({
-  //   id: "mainMenu",
-  //   title: "Main Menu",
-  //   contexts: ["selection"],
-  // });
-  // // コンテンツスクリプトからのメッセージを待ち受ける
-  // browser.runtime.onMessage.addListener((message, sender) => {
-  //   console.log(message);
-  //   if (message.action === "showMenu" && message.selectedText) {
-  //     //   await browser.storage.local.set({ selectedText: info.selectionText });
-  //     // await browser.browserAction.openPopup();
-  //     console.log(message.selectionText);
-  //     // const url = `https://example.com/search?q=${encodedText}`;
-  //     // await browser.tabs.create({ url });
-  //     // サブメニューを作成
-  //     browser.contextMenus.create({
-  //       id: "subMenu",
-  //       parentId: "mainMenu", // メインメニューの下に表示する
-  //       title: message.selectionText,
-  //       contexts: ["selection"],
-  //     });
-  //     // サブメニューがクリックされたときのリスナー
-  //     browser.contextMenus.onClicked.addListener((subInfo, subTab) => {
-  //       if (subInfo.menuItemId === "subMenu" && subInfo.selectionText) {
-  //         console.log("Sub menu clicked:", subInfo.selectionText);
-  //         // ここで選択したテキストに対する処理を行う
+  //   browser.runtime.onMessage.addListener(async function (
+  //     message: any,
+  //     sender: any,
+  //     sendResponse: any
+  //   ) {
+  //     console.log(message);
+  //     if (message.message === "copy") {
+  //       console.log(message.message);
+  //       try {
+  //         const text = await navigator.clipboard.readText();
+  //         console.log(text);
+  //         sendResponse({ result: text });
+  //       } catch (error) {
+  //         console.log(error);
+  //         sendResponse({ error: "error" });
   //       }
-  //     });
-  //   }
-  // });
+  //     }
+  //     // 非同期レスポンスを有効にするために、trueを返す
+  //     return true;
+  //   });
 });
+
+// browser.runtime.onMessage.addListener((message, sender) => {
+//   if (message.action === "getClipboardText") {
+//     readClipboardText().then((text) => {
+//       return text;
+//     });
+//   }
+//   return true; // 非同期レスポンスを返すためにtrueを返す
+// });
+// async function readClipboardText() {
+//   try {
+//     const text = await workerNavigator.clipboard?.readText();
+//     return text;
+//   } catch (error) {
+//     console.error("Failed to read clipboard text:", error);
+//     return null;
+//   }
+// }
+// browser.contextMenus.create({
+//   id: "mainMenu",
+//   title: "Main Menu",
+//   contexts: ["selection"],
+// });
+// // コンテンツスクリプトからのメッセージを待ち受ける
+// browser.runtime.onMessage.addListener((message, sender) => {
+//   console.log(message);
+//   if (message.action === "showMenu" && message.selectedText) {
+//     //   await browser.storage.local.set({ selectedText: info.selectionText });
+//     // await browser.browserAction.openPopup();
+//     console.log(message.selectionText);
+//     // const url = `https://example.com/search?q=${encodedText}`;
+//     // await browser.tabs.create({ url });
+//     // サブメニューを作成
+//     browser.contextMenus.create({
+//       id: "subMenu",
+//       parentId: "mainMenu", // メインメニューの下に表示する
+//       title: message.selectionText,
+//       contexts: ["selection"],
+//     });
+//     // サブメニューがクリックされたときのリスナー
+//     browser.contextMenus.onClicked.addListener((subInfo, subTab) => {
+//       if (subInfo.menuItemId === "subMenu" && subInfo.selectionText) {
+//         console.log("Sub menu clicked:", subInfo.selectionText);
+//         // ここで選択したテキストに対する処理を行う
+//       }
+//     });
+//   }
+// });
+//});
--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,17 +1,65 @@
-import { createSignal, onMount } from "solid-js";
+import { createEffect, createMemo, createSignal, onMount } from "solid-js";
 import "./App.css";
-import { browser } from "wxt/browser";
-function App() {
-  // const [selectedText, setSelectedText] = createSignal("");
 
-  onMount(async () => {
-    // const { selectedText } = await browser.storage.local.get("selectedText");
-    //  setSelectedText(selectedText || "No text selected");
+import { encodableRegex, hexRegex } from "@/util";
+import HexContent from "../content/HexContent";
+import DecodableContent from "../content/DecodableContent";
+function App() {
+  const [selectedText, setSelectedText] = createSignal("");
+
+  // const readClipboardText = () => {
+  //   browser.runtime
+  //     .sendMessage({ action: "getClipboardText" })
+  //     .then((response) => {
+  //       if (response && response.text) {
+  //         // クリップボードから取得したテキストを使用してポップアップの表示を更新するなどの処理を行う
+  //         setSelectedText(response.text);
+  //       }
+  //     });
+  // };
+
+  // onMount(() => {
+  //   console.log(document.visibilityState);
+  //   if (document.visibilityState === "visible") {
+  //     readClipboardText();
+  //   }
+  // });
+
+  // document.addEventListener("visibilitychange", () => {
+  //   console.log(document.visibilityState);
+  //   if (document.visibilityState === "visible") {
+  //     readClipboardText();
+  //   }
+  // });
+
+  const nakeContent = createMemo(() => {
+    // console.log(props.content);
+    // console.log(encodableRegex.test(props.content));
+    //console.log(hexRegex.test(props.content));
+    if (hexRegex.test(selectedText())) {
+      return <HexContent content={selectedText()} />;
+    } else if (encodableRegex.test(selectedText())) {
+      return <DecodableContent content={selectedText()} />;
+    } else {
+      return <div>Invalid content</div>;
+    }
   });
+  // browser.runtime.sendMessage({
+  //   extensionId: browser.runtime.id,
+  //   message: "copy",
+  //   function(response: { result: any }) {
+  //     console.log(response.result);
+  //   },
+  // });
 
   return (
     <div>
-      <h1>Popup</h1>
+      <input
+        type="text"
+        placeholder="hex or note1,npub1,..."
+        onInput={(e) => setSelectedText(e.target.value)}
+      />
+      <div style={{ "margin-top": "1em" }}>{nakeContent()}</div>
     </div>
   );
 }
