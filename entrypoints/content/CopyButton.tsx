7c6ce5532a97a7bd1c5d6e34cc5f8fa0e23736ca
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-05-29 12:54:19 +0900

    fix copyicon

--- entrypoints/content/CopyButton.tsx
+++ entrypoints/content/CopyButton.tsx
@@ -9,6 +9,52 @@ interface CopyButtonProps {
 
 export default function CopyButton({ text, style }: CopyButtonProps) {
   const [copied, setCopied] = createSignal(false);
+
+  const copiedIcon = (
+    <svg
+      class={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+    >
+      <g
+        class={className}
+        fill="none"
+        stroke="#aaa"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+      >
+        <path
+          class={className}
+          d="M8 5H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-2M8 5a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v0M8 5a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2v0"
+        />
+        <path class={className} d="m9 14l2 2l4-5" />
+      </g>
+    </svg>
+  );
+
+  const copyIcon = (
+    <svg
+      class={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+    >
+      <path
+        class={className}
+        fill="none"
+        stroke="#aaa"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M8 5H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-2M8 5a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v0M8 5a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2v0"
+      />
+    </svg>
+  );
+
   const copy = async () => {
     navigator.clipboard.writeText(text);
     setCopied(true);
@@ -30,48 +76,3 @@ export default function CopyButton({ text, style }: CopyButtonProps) {
     </div>
   );
 }
-
-const copiedIcon = (
-  <svg
-    class={className}
-    xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
-    viewBox="0 0 24 24"
-  >
-    <g
-      class={className}
-      fill="none"
-      stroke="#aaa"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
-    >
-      <path
-        class={className}
-        d="M8 5H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-2M8 5a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v0M8 5a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2v0"
-      />
-      <path class={className} d="m9 14l2 2l4-5" />
-    </g>
-  </svg>
-);
-
-const copyIcon = (
-  <svg
-    class={className}
-    xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
-    viewBox="0 0 24 24"
-  >
-    <path
-      class={className}
-      fill="none"
-      stroke="#aaa"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
-      d="M8 5H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-2M8 5a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v0M8 5a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2v0"
-    />
-  </svg>
-);
