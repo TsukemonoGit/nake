8044480c5197697c5624ca20c5377fe141bdf46a
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-05-30 09:43:49 +0900

    fix

--- entrypoints/content/MenuComponent.tsx
+++ entrypoints/content/MenuComponent.tsx
@@ -213,6 +213,7 @@ export default function MenuComponent(props: {
               onTouchStart={handleTouchStart}
             >
               <svg
+                class={`${className}`}
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
                 height="24"
